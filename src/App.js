bfcf22d510d2e1c52da0203081efa043612b23b9
kudaT <timilehinkudaisi01@gmail.com>
2023-02-10 15:07:35 +0100

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter } from "react-router-dom"
 import "./App.css";
 import Loyal from "./Loyal";
 import Loyal2 from "./Loyal2";
@@ -5,8 +6,10 @@ import Loyal2 from "./Loyal2";
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Loyal />
       <Loyal2 />
+      </BrowserRouter>
     </div>
   );
 }
